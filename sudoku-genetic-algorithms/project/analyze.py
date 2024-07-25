303336d340159c089507eccc75ffadf142a82d75
TalkySafe143 <74377523+TalkySafe143@users.noreply.github.com>
2024-07-25 15:41:22 -0500

    Demo 2/2

--- sudoku-genetic-algorithms/project/analyze.py
+++ sudoku-genetic-algorithms/project/analyze.py
@@ -1,11 +1,4 @@
 from contextlib import contextmanager
 import time
 
-# Explicar porque @contextmanager y explicar lo de las funciones generadoras 
-# Escribir esta función estaría bien
-@contextmanager
-def timer():
-    start = time.time()
-    yield
-    end = time.time()
-    print(f"Elapsed Time: {(end - start)}s")
+# 
